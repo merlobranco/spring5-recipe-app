b45fc94198f999e267be1e0f9888fb87725a7152
Brais Cidras <braisomary@gmail.com>
2021-04-17 20:42:03 +0200

    Using Project Lombok

--- src/main/java/merlobranco/springframework/services/RecipeServiceImpl.java
+++ src/main/java/merlobranco/springframework/services/RecipeServiceImpl.java
@@ -7,9 +7,11 @@ import java.util.Set;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.extern.slf4j.Slf4j;
 import merlobranco.springframework.domain.Recipe;
 import merlobranco.springframework.repositories.RecipeRepository;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -22,6 +24,7 @@ public class RecipeServiceImpl implements RecipeService {
     @Override
     @Transactional(readOnly=true)
     public Set<Recipe> getRecipes() {
+    	log.debug("I'm in the service");
     	return new HashSet<Recipe>((Collection<? extends Recipe>) recipeRepository.findAll());
     }
 }
