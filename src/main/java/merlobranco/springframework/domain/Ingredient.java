c81d5a4554a9e2d032370cbefb3dc1473dfd2a03
Brais Cidras <braisomary@gmail.com>
2021-04-19 18:44:14 +0200

    Using Bootstrap CSS

--- src/main/java/merlobranco/springframework/domain/Ingredient.java
+++ src/main/java/merlobranco/springframework/domain/Ingredient.java
@@ -14,6 +14,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
+
 @EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
@@ -30,6 +31,8 @@ public class Ingredient {
 	
 	@ManyToOne
 	private Recipe recipe;
+	
+	public Ingredient() {}
 
 	public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
 		this.description = description;
