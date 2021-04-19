65ad03ff8314de2b5d862dc92fa19dc63a009b4f
Brais Cidras <braisomary@gmail.com>
2021-04-19 20:50:07 +0200

    Spring Integration Test

---
+++ src/test/java/merlobranco/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,36 @@
+package merlobranco.springframework.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import merlobranco.springframework.domain.UnitOfMeasure;
+
+
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest // Will bring an embedded database and will configure Spring data JPA for us
+class UnitOfMeasureRepositoryIT {
+	
+	@Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@Test
+	void testFindByDescription() {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		assertEquals("Teaspoon", uomOptional.get().getDescription());
+	}
+	
+	@Test
+    public void findByDescriptionCup() throws Exception {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+
+}
