2707673a3338b7162045d246f830f3109837b927
Brais Cidras <braisomary@gmail.com>
2021-04-19 20:29:23 +0200

    Adding Mockito Argument Capture

--- src/test/java/merlobranco/springframework/controllers/IndexControllerTest.java
+++ src/test/java/merlobranco/springframework/controllers/IndexControllerTest.java
@@ -5,13 +5,19 @@ import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import merlobranco.springframework.domain.Recipe;
 import merlobranco.springframework.services.RecipeService;
 
 class IndexControllerTest {
@@ -32,9 +38,29 @@ class IndexControllerTest {
 
 	@Test
 	void testGetIndexPage() {
-		assertEquals("index", indexController.getIndexPage(model));
+		// Given
+		Set<Recipe> recipes = new HashSet<>();
+		recipes.add(new Recipe());
+		
+		// We cannot pass again a recipe with empty id
+		// It will be considered the same as the previous one
+		Recipe recipe = new Recipe();
+		recipe.setId(1L);
+		recipes.add(recipe);
+		
+		when(recipeService.getRecipes()).thenReturn(recipes);
+		
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+		
+		// When
+		String viewName = indexController.getIndexPage(model);
+		
+		// Then
+		assertEquals("index", viewName);
 		verify(recipeService, times(1)).getRecipes();
-		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+		Set<Recipe> setInController = argumentCaptor.getValue();
+		assertEquals(2, setInController.size());
 	}
 
 }
