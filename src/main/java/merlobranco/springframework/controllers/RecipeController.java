22c95fcf6a4c0c263e63d466cd6518a41e759d34
Brais Cidras <braisomary@gmail.com>
2021-06-23 19:41:31 +0200

    Create a Recipe

--- src/main/java/merlobranco/springframework/controllers/RecipeController.java
+++ src/main/java/merlobranco/springframework/controllers/RecipeController.java
@@ -3,9 +3,12 @@ package merlobranco.springframework.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import merlobranco.springframework.commands.RecipeCommand;
 import merlobranco.springframework.services.RecipeService;
 
 @Controller
@@ -23,5 +26,18 @@ public class RecipeController {
 		model.addAttribute("recipe", recipeService.findById(id));
 		return "recipe/show";
 	}
-
+	
+	
+	@GetMapping("/form")
+    public String create(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeForm";
+    }
+	
+	
+	@PostMapping("/")
+    public String save(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
 }
